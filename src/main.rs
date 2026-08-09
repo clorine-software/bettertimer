3d2283618c500b12e183e37531a2d8eca6bca3b5
slawacats <slawacats@gmail.com>
2026-08-09 20:07:43 +0300

    v1.0.0 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use indicatif::{ProgressBar, ProgressStyle};
+use tokio::time::{Duration, sleep, interval};
+use notify_rust::Notification;
+use clap::Parser;
+use humantime::{format_duration, parse_duration};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    time: String,
+
+    #[arg(short, long, default_value_t = false)]
+    silent: bool,
+
+    #[arg(short, long, default_value_t = String::from("Time's up"))]
+    message: String,
+}
+
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let total = match parse_duration(&args.time) {
+        Ok(t) => t,
+        Err(e) => {println!("Ошибка парсинга времени: {}", e); panic!()},
+    };
+
+    let start = tokio::time::Instant::now();
+
+    let timer = sleep(total);
+    tokio::pin!(timer);
+
+    let formatted_total = format_duration(total);
+
+    let bar = ProgressBar::new(total.as_millis() as u64).with_message(format!("{}", formatted_total));
+    bar.set_style(ProgressStyle::with_template("[{msg}] {bar:40.cyan/blue} {percent}%")
+        .unwrap()
+    );
+
+    let mut interval = interval(Duration::from_millis(10));
+    loop {
+        tokio::select! {
+            _ = &mut timer => {
+                bar.set_position(total.as_millis() as u64);
+                bar.set_message(format!("{}/{}", formatted_total, formatted_total));
+                bar.finish();
+                break
+            },
+            _ = interval.tick() => {
+                let elapsed = start.elapsed();
+                bar.set_position(elapsed.as_millis() as u64);
+                bar.set_message(format!("{}/{}", format_duration(Duration::from_millis(elapsed.as_millis() as u64)), formatted_total));
+            }
+        }
+    }
+   
+    if !args.silent { Notification::new().summary(&args.message).show_async().await.unwrap(); }
 }
